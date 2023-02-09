bd6e82548ea332d7fb946b2ab452d9ae30b16821
Dmitry Stetsenko <ebusines2010@gmail.com>
2023-02-09 19:38:38 +0200

    add basic menu

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let balance = 1000;
+let currency = 'uah';
+
+while (true) {
+    const userChoose = prompt('1. balance, 2.get money, 3.put money, 4.exit');
+    if (userChoose == '1') {
+        getBalance();
+    } else if (userChoose == '2') {
+        getMoney();
+    } else if (userChoose == '3') {
+        gputMoney()
+    } else if (userChoose == '4') {
+        break;
+    } else {
+        alert('Error type');
+    }
+}
+
+
+function getBalance() {
+    alert(balance);
+}
+function getMoney() {
+    alert('getMoney');
+}
+function putMoney() {
+    alert('putMoney');
+}
