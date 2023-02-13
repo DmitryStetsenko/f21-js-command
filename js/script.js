465855ad9623da7192d10ead0ab9c7461c668f45
Olesia <okavkoo@gmail.com>
2023-02-13 15:06:53 +0000

    hide put money block

--- js/script.js
+++ js/script.js
@@ -1,223 +1,222 @@
 //Vova
-localStorage.setItem('links', JSON.stringify(
-    {
-        id: 1,
-        name: 'User1',
-        pincode: 1111
-    }
-));
+localStorage.setItem(
+  "links",
+  JSON.stringify({
+    id: 1,
+    name: "User1",
+    pincode: 1111,
+  })
+);
 //
 
-const user = JSON.parse(localStorage.getItem('links')); //Vova
+const user = JSON.parse(localStorage.getItem("links")); //Vova
 const putBtn = document.querySelector(".put-btn"); //Vova
 const changeBtn = document.querySelector(".change-btn"); //Vova
-const takeMoneyBtn = document.querySelector('.takeMoneyBtn');
+const takeMoneyBtn = document.querySelector(".takeMoneyBtn");
 let currency = document.querySelector(".currency");
 let deposite = document.querySelector(".deposite");
-let pin = user.pincode;//Vova
+let pin = user.pincode; //Vova
+const putMoneyBlock = document.querySelector(".put-money-block");
 
 let rate;
-let currCurrency = 'UAH';
-let newCurr =' ';
-let displayBalance = document.querySelector('.balance');
+let currCurrency = "UAH";
+let newCurr = " ";
+let displayBalance = document.querySelector(".balance");
 
 let balance = 0;
 
 putBtn.addEventListener("click", putMoney);
 
 //Daniil
-document.querySelector('.USD').addEventListener('click', (() => {
-    changeCurr('USD', currCurrency, newCurr, balance);
-    currCurrency = 'USD';
-    console.log(newCurr);
-
-}));
-document.querySelector('.EUR').addEventListener('click',(() => {
-    changeCurr('EUR', currCurrency, newCurr, balance);
-    currCurrency = 'EUR';
-    console.log(newCurr);
-
-}));
-document.querySelector('.UAH').addEventListener('click',(() => {
-    changeCurr('UAH', currCurrency, newCurr, balance);
-    console.log(newCurr);
-    currCurrency = 'UAH';
-}));
+document.querySelector(".USD").addEventListener("click", () => {
+  changeCurr("USD", currCurrency, newCurr, balance);
+  currCurrency = "USD";
+  console.log(newCurr);
+});
+document.querySelector(".EUR").addEventListener("click", () => {
+  changeCurr("EUR", currCurrency, newCurr, balance);
+  currCurrency = "EUR";
+  console.log(newCurr);
+});
+document.querySelector(".UAH").addEventListener("click", () => {
+  changeCurr("UAH", currCurrency, newCurr, balance);
+  console.log(newCurr);
+  currCurrency = "UAH";
+});
 //
 
 //Vova
-changeBtn.onclick = function() {
-    changePinCode(pin);
-}
+changeBtn.onclick = function () {
+  changePinCode(pin);
+};
 //
 
 // Maxym
 
 const crrncy = {
-    'EUR': {'UAH': 40, 'USD': 0.83, 'GBP': 0.7}, 
-    'USD': {'UAH': 41, 'EUR': 1.2, 'GBP': 0.7},
-    'UAH': {'EUR': 0.024, 'USD': 0.023, 'GBP': 0.022},
-    'GBP': {'UAH': 45, 'USD': 1.3, 'EUR': 1.4}
-}
+  EUR: { UAH: 40, USD: 0.83, GBP: 0.7 },
+  USD: { UAH: 41, EUR: 1.2, GBP: 0.7 },
+  UAH: { EUR: 0.024, USD: 0.023, GBP: 0.022 },
+  GBP: { UAH: 45, USD: 1.3, EUR: 1.4 },
+};
 
-const btn = document.querySelector('.calculate-btn');
-const baseCurrencyInput = document.getElementById('currency-1');
-const secondCurrencyInput = document.getElementById('currency-2');
-const amountInput = document.getElementById('amount');
-const toShowAmount = document.querySelector('.given-amount');
-const toShowBase = document.querySelector('.base-currency');
-const toShowSecond = document.querySelector('.second-currency');
-const toShowResult = document.querySelector('.final-result');
-
+const btn = document.querySelector(".calculate-btn");
+const baseCurrencyInput = document.getElementById("currency-1");
+const secondCurrencyInput = document.getElementById("currency-2");
+const amountInput = document.getElementById("amount");
+const toShowAmount = document.querySelector(".given-amount");
+const toShowBase = document.querySelector(".base-currency");
+const toShowSecond = document.querySelector(".second-currency");
+const toShowResult = document.querySelector(".final-result");
+const putMoneyBtn = document.querySelector(".put-money-btn");
 // Maxym
 
 function auth() {
-    console.log("auth");
+  console.log("auth");
 }
 function menu() {
-    console.log("menu");
+  console.log("menu");
 }
 
 function getBalance() {
-    console.log("getBalance");
+  console.log("getBalance");
 }
 
-takeMoneyBtn.addEventListener('click', function(){
-    getMoney();
-})
+takeMoneyBtn.addEventListener("click", function () {
+  getMoney();
+});
 
 function getMoney() {
-    console.log("getMoney");
-    console.log(balance);
-    alert('Ваш баланс ' + balance + ' UAH');
-    let curencyCheck = prompt('Какую валюту вы хотите снять? (UAH, USD, EUR, GBP)', '');
+  console.log("getMoney");
+  console.log(balance);
+  alert("Ваш баланс " + balance + " UAH");
+  let curencyCheck = prompt(
+    "Какую валюту вы хотите снять? (UAH, USD, EUR, GBP)",
+    ""
+  );
 
-    if(curencyCheck == 'USD'){
-        let balanceUSD = (balance / 40);
-        console.log(balanceUSD);
-        cash = prompt('Сколько наличных вы хотите снять?', '');
-        alert(`Вы хотите снять ${cash} usd`);
-        console.log(cash);
-        if(cash > balanceUSD){
-        alert("Недостаточный баланс");
-        } else if (cash <= balanceUSD){
-        alert('Возьмите ваши наличные')
-        }else{
-        alert('ERROR 404');
-        }
-    
-    }else if(curencyCheck == 'UAH'){
-        //console.log(currency.uah);
-        cash = prompt('Сколько наличных вы хотите снять?', '');
-        alert(`Вы хотите снять ${cash} uah`);
-        console.log(cash);
-        if(cash > balance){
-         alert("Недостаточный баланс");
-        } else if (cash <= balance){
-         alert('Возьмите ваши наличные')
-        }else{
-            alert('ERROR 404');
-        }
-    
-    }else if(curencyCheck == 'EUR'){
-        //console.log(currency.eur);
-        let balanceEUR = (balance / 41);
-        cash = prompt('Сколько наличных вы хотите снять?', '');
-        alert(`Вы хотите снять ${cash} eur`);
-        console.log(cash);
-        if(cash > balanceEUR){
-         alert("Недостаточный баланс");
-        } else if (cash <= balanceEUR){
-         alert('Возьмите ваши наличные')
-        }else{
-            alert('ERROR 404');
-        }
-    
-    }else if(curencyCheck == 'gbp'){
-        //console.log(currency.gbp);
-        let balanceGBP = (balance / 42);
-        console.log(balanceGBP);
-        cash = prompt('Сколько наличных вы хотите снять?', '');
-        alert(`Вы хотите снять ${cash} gbp`);
-        console.log(cash);
-        if(cash > balanceGBP){
-         alert("Недостаточный баланс");
-        } else if (cash <= balanceGBP){
-         alert('Возьмите ваши наличные')
-        }else{
-            alert('ERROR 404');
-        }
-    
-    }else{
-        console.log('Wrong Curency!!!');
+  if (curencyCheck == "USD") {
+    let balanceUSD = balance / 40;
+    console.log(balanceUSD);
+    cash = prompt("Сколько наличных вы хотите снять?", "");
+    alert(`Вы хотите снять ${cash} usd`);
+    console.log(cash);
+    if (cash > balanceUSD) {
+      alert("Недостаточный баланс");
+    } else if (cash <= balanceUSD) {
+      alert("Возьмите ваши наличные");
+    } else {
+      alert("ERROR 404");
     }
-
+  } else if (curencyCheck == "UAH") {
+    //console.log(currency.uah);
+    cash = prompt("Сколько наличных вы хотите снять?", "");
+    alert(`Вы хотите снять ${cash} uah`);
+    console.log(cash);
+    if (cash > balance) {
+      alert("Недостаточный баланс");
+    } else if (cash <= balance) {
+      alert("Возьмите ваши наличные");
+    } else {
+      alert("ERROR 404");
+    }
+  } else if (curencyCheck == "EUR") {
+    //console.log(currency.eur);
+    let balanceEUR = balance / 41;
+    cash = prompt("Сколько наличных вы хотите снять?", "");
+    alert(`Вы хотите снять ${cash} eur`);
+    console.log(cash);
+    if (cash > balanceEUR) {
+      alert("Недостаточный баланс");
+    } else if (cash <= balanceEUR) {
+      alert("Возьмите ваши наличные");
+    } else {
+      alert("ERROR 404");
+    }
+  } else if (curencyCheck == "gbp") {
+    //console.log(currency.gbp);
+    let balanceGBP = balance / 42;
+    console.log(balanceGBP);
+    cash = prompt("Сколько наличных вы хотите снять?", "");
+    alert(`Вы хотите снять ${cash} gbp`);
+    console.log(cash);
+    if (cash > balanceGBP) {
+      alert("Недостаточный баланс");
+    } else if (cash <= balanceGBP) {
+      alert("Возьмите ваши наличные");
+    } else {
+      alert("ERROR 404");
+    }
+  } else {
+    console.log("Wrong Curency!!!");
+  }
 }
 
 //<olesia>
 
 function putMoney() {
-    rate = currency.value;
-    balance = deposite.value * rate;
-    console.log(balance);
-    displayBalance.innerText = balance;
+  rate = currency.value;
+  balance += deposite.value * rate;
+  deposite.value = "";
+  displayBalance.innerText = balance;
 }
+putMoneyBtn.onclick = () => {
+  putMoneyBlock.classList.remove("none");
+};
 
 //</olesia>
 
 //Daniil
-function changeCurr(currentCurr, changedCur, currency, balance){
-    const currencyRate = {
-        USD: 36.72,
-        EUR: 39.51,
-    }
-    if (changedCur == 'UAH'){
-        currency = `${balance * currencyRate[currentCurr]} ${changedCur}`;
-        balance = balance * currencyRate[currentCurr];
-        console.log(currency)
-    }
-    else{
-        currency = `${balance * (1/currencyRate[currentCurr])} ${changedCur}`
-        balance = balance * (1/currencyRate[currentCurr]);
-        console.log(currency);
-    }
+function changeCurr(currentCurr, changedCur, currency, balance) {
+  const currencyRate = {
+    USD: 36.72,
+    EUR: 39.51,
+  };
+  if (changedCur == "UAH") {
+    currency = `${balance * currencyRate[currentCurr]} ${changedCur}`;
+    balance = balance * currencyRate[currentCurr];
+    console.log(currency);
+  } else {
+    currency = `${balance * (1 / currencyRate[currentCurr])} ${changedCur}`;
+    balance = balance * (1 / currencyRate[currentCurr]);
+    console.log(currency);
+  }
 }
 
 // Maxym
 
 function currCalc(event) {
-    event.preventDefault();
-    let amount = amountInput.value;
-    let from = baseCurrencyInput.value;
-    let to = secondCurrencyInput.value;
-    let letresult = 0;
-    
-    try{
-      if (from == to){
-        result = amount;
-      } else {
-       result = amount * crrncy[from][to];
+  event.preventDefault();
+  let amount = amountInput.value;
+  let from = baseCurrencyInput.value;
+  let to = secondCurrencyInput.value;
+  let letresult = 0;
+
+  try {
+    if (from == to) {
+      result = amount;
+    } else {
+      result = amount * crrncy[from][to];
     }
-    }
-    catch(err) {
-      result = amount * (1 / crrncy[to][from]);
-    }
-    
-    toShowAmount.innerHTML = amount;
-    toShowBase.textContent = from + ' = ';
-    toShowSecond.textContent = to;
-    toShowResult.textContent = result; 
+  } catch (err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + " = ";
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result;
 }
-  
-btn.addEventListener('click', currCalc);
+
+btn.addEventListener("click", currCalc);
 
 // Maxym
 
 //Vova
 function changePinCode(pincode) {
-    localStorage.setItem("pin", `${Number(prompt("Введите новый пин-код"))}`);
-    pincode = localStorage.getItem("pin");
-    console.log(pincode);
-    return pincode;
+  localStorage.setItem("pin", `${Number(prompt("Введите новый пин-код"))}`);
+  pincode = localStorage.getItem("pin");
+  console.log(pincode);
+  return pincode;
 }
 //
